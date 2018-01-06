ed38242c1d4809b60359d906c0323a6b6ceba2b2
Vesa Karvonen <vesa.a.j.k@gmail.com>
2018-01-06 15:51:09 +0200

    Fix deprecation

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,21 @@
-import babel       from "rollup-plugin-babel"
-import nodeResolve from "rollup-plugin-node-resolve"
-import replace     from "rollup-plugin-replace"
-import uglify      from "rollup-plugin-uglify"
+import babel from 'rollup-plugin-babel'
+import nodeResolve from 'rollup-plugin-node-resolve'
+import replace from 'rollup-plugin-replace'
+import uglify from 'rollup-plugin-uglify'
 
 export default {
-  external: [
-    "infestines",
-    "partial.lenses"
-  ],
-  globals: {
-    "infestines": "I",
-    "partial.lenses": "L"
+  external: ['infestines', 'partial.lenses'],
+  output: {
+    globals: {
+      infestines: 'I',
+      'partial.lenses': 'L'
+    }
   },
   plugins: [
     process.env.NODE_ENV &&
-      replace({"process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV)}),
-    nodeResolve({modulesOnly: true}),
+      replace({ 'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV) }),
+    nodeResolve({ modulesOnly: true }),
     babel(),
-    process.env.NODE_ENV === "production" &&
-      uglify()
+    process.env.NODE_ENV === 'production' && uglify()
   ].filter(x => x)
 }
